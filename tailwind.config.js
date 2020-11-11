d4cb6d0240896ade5420d15ba7dee1fe707db152
apandit42 <apandit@stanford.edu>
2020-11-11 01:27:16 -0600

    tailwind approach

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: {
+    mode: 'all',
+    content: [
+      './*.html',
+    ],
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
